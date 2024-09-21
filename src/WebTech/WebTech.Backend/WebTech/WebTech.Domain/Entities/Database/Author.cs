b141f0aea9ab46253364a7da43158fcae92b8a9f
Ivan <zhmur.ivan@gmail.com>
2024-09-21 21:50:23 +0300

    chore(backend): add token controller

--- src/WebTech/WebTech.Backend/WebTech/WebTech.Domain/Entities/Database/Author.cs
+++ src/WebTech/WebTech.Backend/WebTech/WebTech.Domain/Entities/Database/Author.cs
@@ -5,17 +5,11 @@ namespace WebTech.Domain.Entities.Database;
 
 public class Author : DatabaseEntity
 {
-    [Required]
-    [MaxLength(48)]
-    [MinLength(3)]
     public string FirstName { get; set; }
 
-    [Required]
-    [MaxLength(48)]
-    [MinLength(3)]
     public string LastName { get; set; }
 
-    [Required] public DateTime BirthDate { get; set; }
+    public DateTime BirthDate { get; set; }
 
     [JsonIgnore] public ICollection<Book> Books = new List<Book>();
 }
--- src/WebTech/WebTech.Backend/WebTech/WebTech.Domain/Entities/Database/Book.cs
+++ src/WebTech/WebTech.Backend/WebTech/WebTech.Domain/Entities/Database/Book.cs
@@ -5,17 +5,11 @@ namespace WebTech.Domain.Entities.Database;
 
 public class Book : DatabaseEntity
 {
-    [Required]
-    [MaxLength(128)]
-    [MinLength(3)]
     public string Title { get; set; }
 
-    [Required]
-    [MaxLength(48)]
-    [MinLength(3)]
     public string Genre { get; set; }
 
-    [Required] public int PublicationYear { get; set; }
+    public int PublicationYear { get; set; }
 
     public Guid AuthorId { get; set; }
 
