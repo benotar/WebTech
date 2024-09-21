42f5b0318e8a58841cb800989398c5c1770b46b6
Ivan <zhmur.ivan@gmail.com>
2024-09-21 22:02:52 +0300

    feat(backend): add expression extensions

---
+++ src/WebTech/WebTech.Backend/WebTech/WebTech.Application/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace WebTech.Application.Extensions;
+
+public static class ExpressionExtensions
+{
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first,
+        Expression<Func<T, bool>> second)
+    {
+        var invokedExpression = Expression.Invoke(second, first.Parameters);
+
+        var body = Expression.AndAlso(first.Body, invokedExpression);
+
+        return Expression.Lambda<Func<T, bool>>(body, first.Parameters);
+    }
+}
